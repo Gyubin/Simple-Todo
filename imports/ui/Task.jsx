c6d0257777a75a41003984a7172dced370336437
Gyubin Son <geubin0414@gmail.com>
2016-11-29 21:44:49 -0800

    publish, subscribe

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -9,9 +9,16 @@ export default class Task extends Component {
   deleteThisTask() {
     Meteor.call('tasks.remove', this.props.task._id);
   }
-  
+
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+  }
+
   render () {
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
     return (
       <li className={taskClassName}>
         <button className="delete" onClick={this.deleteThisTask.bind(this)}>
@@ -24,7 +31,11 @@ export default class Task extends Component {
           checked={this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}
         />
-
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.task.private ? 'Private' : 'Public' }
+          </button>
+        ) : ''}
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}
         </span>
@@ -35,4 +46,5 @@ export default class Task extends Component {
 
 Task.propTypes = {
   task: PropTypes.object.isRequired,
+  showPrivateButton: React.PropTypes.bool.isRequired,
 };
